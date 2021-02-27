102a23e5573266a08740911e0b55f158dadad7c7
mollnn <1084918073@qq.com>
2021-02-27 15:22:45 +0800

    feat specular

--- include/vec3.hpp
+++ include/vec3.hpp
@@ -17,6 +17,7 @@ struct vec3
     double norm2() { return x * x + y * y + z * z; }
     double norm() { return sqrt(norm2()); }
     vec3 unit() { return (*this) / norm(); }
+    double avg() { return (x + y + z) / 3; }
 };
 vec3 operator*(double lhs, const vec3 &rhs) { return {lhs * rhs.x, lhs * rhs.y, lhs * rhs.z}; }
 
--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,8 @@ uniform_int_distribution<int> global_uniform_int_distribution(0, RAND_MAX);
 
 struct Material
 {
-	vec3 diffuse, emission;
+	vec3 diffuse, reflect, refrect, emission;
+	double refrect_index;
 };
 
 struct Triangle
@@ -40,7 +41,6 @@ struct Triangle
 		if (x < 0)
 			return {-1, vec3()};
 		vec3 ph = pos + x * dir;
-		double dis = (ph - p0).dot(normal());
 		vec3 q0 = (p1 - p0).cross(ph - p0);
 		vec3 q1 = (p2 - p1).cross(ph - p1);
 		vec3 q2 = (p0 - p2).cross(ph - p2);
@@ -75,28 +75,89 @@ std::tuple<double, vec3, Triangle *> intersect(std::vector<Triangle> &triangles,
 
 vec3 PathTrace(vec3 raypos, vec3 raydir, int depth, std::vector<Triangle> &triangles)
 {
-	if (depth > 10)
+	if (depth > 20)
 		return {0, 0, 0};
 	auto [hitdis, hitpos, hitobj] = intersect(triangles, raypos, raydir);
 	if (hitdis < 0)
 		return {0, 0, 0};
-	double r2 = randf();
-	double phi = randf() * 2 * pi;
-	double sqr2 = sqrt(r2);
-	// double cosphi = cos(phi);
-	double cosphi = fastmath::get_cos_l(phi);
-	double sinphi = sqrt(1 - cosphi * cosphi) * (phi < pi ? 1 : -1);
-	double du = sqr2 * cosphi;
-	double dv = sqr2 * sinphi;
-	double dw = sqrt(1 - r2);
-	vec3 normal = hitobj->normal();
-	if (raydir.dot(normal) > 0)
-		normal = -1 * normal;
-	vec3 ew = normal;
-	vec3 eu = ((vec3){randf(), randf(), randf()}).unit().cross(normal);
-	vec3 ev = eu.cross(ew);
-	vec3 difdir = du * eu + dv * ev + dw * ew;
-	return hitobj->mat.emission + hitobj->mat.diffuse * PathTrace(hitpos + eps * difdir, difdir, depth + 1, triangles);
+
+	vec3 normal_outward = hitobj->normal();
+	bool is_into = raydir.dot(normal_outward) < 0;				  // 是否在射入内部
+	vec3 normal = is_into ? normal_outward : -1 * normal_outward; // 实际光学效应的法线
+	double relative_refract_index = is_into ? hitobj->mat.refrect_index : 1.0 / hitobj->mat.refrect_index;
+
+	vec3 in_dir = raydir;								  // 光线入射方向
+	double cos_i = abs(in_dir.dot(normal));				  // 入射角 cos
+	double sin_i = sqrt(1 - cos_i * cos_i);				  // 入射角 sin
+	double sin_j = sin_i / relative_refract_index;		  // 折射角 sin
+	double cos_j = sqrt(1 - sin_j * sin_j);				  // 折射角 cos
+	vec3 base_dir = (in_dir + cos_i * normal).unit();	  // 入射面中表面的切线方向
+	vec3 reflect_dir = (in_dir + 2 * cos_i * normal);	  // 反射光线方向
+	vec3 refrect_dir = sin_j * base_dir - cos_j * normal; // 折射光线方向
+
+	double refrect_index_in = is_into ? 1.0 : hitobj->mat.refrect_index;								   // 入射介质折射率
+	double refrect_index_out = is_into ? hitobj->mat.refrect_index : 1.0;								   // 折射介质折射率
+	double refrect_index_sum = refrect_index_in + refrect_index_out;									   // 折射率之和
+	double refrect_index_delta = refrect_index_in - refrect_index_out;									   // 折射率之差
+	double fresnel_i0 = refrect_index_delta * refrect_index_delta / refrect_index_sum / refrect_index_sum; // 菲涅尔反射光强系数
+	double fresnel_x = 1 - cos_i;																		   // 菲涅尔反射光强因子
+
+	// 特殊处理全反射情况
+	if (sin_j > 1 - 1e-6)
+		fresnel_x = 1;
+	if (isnanf(fresnel_x))
+		fresnel_x = 1;
+
+	Material &material = hitobj->mat; // 命中物体的材质
+
+	double fresnel_reflect_intensity = fresnel_i0 + (1 - fresnel_i0) * pow(fresnel_x, 5); // 菲涅尔反射强度
+	double fresnel_refrect_intensity = 1 - fresnel_reflect_intensity;					  // 菲涅尔折射强度
+
+	// 搜索各种光线的概率
+	double reflect_probability = material.reflect.avg() + material.refrect.avg() * fresnel_reflect_intensity; // 总镜面反射概率
+	double refrect_probability = material.refrect.avg() * fresnel_refrect_intensity;						  // 总折射概率
+	double diffuse_probability = material.diffuse.avg();													  // 总漫反射概率
+
+	double sum_probability = diffuse_probability + reflect_probability + refrect_probability + eps;
+	diffuse_probability /= sum_probability;
+	reflect_probability /= sum_probability;
+	refrect_probability /= sum_probability;
+
+	// 光线强度
+	vec3 reflect_intensity = material.reflect + material.refrect * fresnel_reflect_intensity; // 总镜面反射强度
+	vec3 refrect_intensity = material.refrect * fresnel_refrect_intensity;					  // 总折射强度
+	vec3 diffuse_intensity = material.diffuse;												  // 总漫反射强度
+
+	vec3 ans = hitobj->mat.emission;
+
+	double rand_value = randf();
+	if (rand_value < diffuse_probability)
+	{
+		double r2 = randf();
+		double phi = randf() * 2 * pi;
+		double sqr2 = sqrt(r2);
+		double cosphi = fastmath::get_cos_l(phi);
+		double sinphi = sqrt(1 - cosphi * cosphi) * (phi < pi ? 1 : -1);
+		double du = sqr2 * cosphi;
+		double dv = sqr2 * sinphi;
+		double dw = sqrt(1 - r2);
+		if (raydir.dot(normal) > 0)
+			normal = -1 * normal;
+		vec3 ew = normal;
+		vec3 eu = ((vec3){randf(), randf(), randf()}).unit().cross(normal);
+		vec3 ev = eu.cross(ew);
+		vec3 difdir = du * eu + dv * ev + dw * ew;
+		ans = ans + hitobj->mat.diffuse * PathTrace(hitpos + eps * difdir, difdir, depth + 1, triangles);
+	}
+	else if (rand_value - diffuse_probability < reflect_probability)
+	{
+		ans = ans + PathTrace(hitpos + eps * reflect_dir, reflect_dir, depth + 1, triangles) * reflect_intensity / reflect_probability;
+	}
+	else if (rand_value - diffuse_probability - reflect_probability < refrect_probability)
+	{
+		ans = ans + PathTrace(hitpos + eps * refrect_dir, refrect_dir, depth + 1, triangles) * refrect_intensity / refrect_probability;
+	}
+	return ans;
 }
 
 void render_thread(int img_siz_x, int img_siz_y, int spp, vec3 cam_pos, vec3 cam_dir, vec3 cam_top, double focal, double near_clip,
@@ -132,7 +193,7 @@ void render(int img_siz_x, int img_siz_y, int spp, vec3 cam_pos, vec3 cam_dir, v
 	double fp_siz_y = fp_siz_x * img_siz_y / img_siz_x;
 	vec3 fp_e_y = cam_top;
 	vec3 fp_e_x = cam_dir.cross(fp_e_y);
-	int img_y_step = 32;
+	int img_y_step = (img_siz_y + 7) / 8;
 
 	vector<thread *> thread_list;
 	for (int img_y = 0; img_y < img_siz_y; img_y += img_y_step)
@@ -163,11 +224,14 @@ int main(int argc, char *argv[])
 
 	std::vector<Triangle> scene;
 
-	scene.push_back({{-0.7, 0, 0}, {0.7, 0, 0}, {0, 0, 1.8}, {{0.8, 0.8, 0.8}, {0, 0, 0}}});	  // 演员
-	scene.push_back({{-5, -7, 0}, {5, -7, 0}, {0, -7, 5}, {{0, 0, 0}, {4.5, 2, 0}}});			  // 射灯
-	scene.push_back({{-1e2, 1e2, 0}, {1e2, 1e2, 0}, {0, -1e2, 0}, {{0.3, 0.3, 0.3}, {0, 0, 0}}}); // 地板
-	scene.push_back({{-10, -10, 3}, {10, -10, 3}, {0, -5, 5}, {{0, 0, 0}, {1, 1.5, 2.2}}});		  // 背景灯
-	scene.push_back({{-1e2, 2, 0}, {1e2, 2, 0}, {0, 2, 1e3}, {{0.5, 0.5, 0.5}, {0, 0, 0}}});	  // 幕布
+	scene.push_back({{-0.7 - 0.5, 0, 0}, {0.7 - 0.5, 0, 0}, {0 - 0.5, 0, 1.8}, {{0.9, 0.9, 0.8}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, 1}});						 // 演员
+	scene.push_back({{-5, -7, 0}, {5, -7, 0}, {0, -7, 5}, {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {4.5, 2.1, 0}, 1}});											 // 射灯
+	scene.push_back({{-1e2, 1e2, 0}, {1e2, 1e2, 0}, {0, -1e2, 0}, {{0.3, 0.32, 0.4}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, 1}});									 // 地板
+	scene.push_back({{-10, -10, 3}, {10, -10, 3}, {0, -5, 5}, {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {1, 1.5, 2.5}, 1}});										 // 背景灯
+	scene.push_back({{-1e2, 2, 0}, {1e2, 2, 0}, {0, 2, 1e3}, {{0.5, 0.6, 0.6}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, 1}});										 // 幕布
+	scene.push_back({{2 + 0.00 * 3, 2, 0}, {2 + 0.01 * 3, 1.9, 0}, {2 + 0.00 * 3, 2, 1e3}, {{0.3, 0.2, 0.2}, {0.3, 0.2, 0.2}, {0, 0, 0}, {0, 0, 0}, 1}});	 // 镜子
+	scene.push_back({{2 + 0.49 * 3, -2.9, 0}, {2 + 0.50 * 3, -3, 0}, {2 + 0.50 * 3, -3, 1e3}, {{0.3, 0.2, 0.2}, {0.3, 0.2, 0.2}, {0, 0, 0}, {0, 0, 0}, 1}}); // 镜子
+	scene.push_back({{2 + 0.49 * 3, -2.9, 0}, {2 + 0.01 * 3, 1.9, 0}, {2 + 0.20 * 3, 0, 1e3}, {{0.1, +.1, 0.1}, {0.6, 0.6, 0.6}, {0, 0, 0}, {0, 0, 0}, 1}}); // 镜子
 
 	for (auto &i : scene)
 		i.auto_normal();
