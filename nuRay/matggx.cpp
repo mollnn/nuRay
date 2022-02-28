4c05ec840235f0e01e4335294ecb5c19433bd05f
mollnn <1084918073@qq.com>
2022-02-28 16:28:34 +0800

    feat: ggx bsdf sampler

--- nuRay/matggx.cpp
+++ nuRay/matggx.cpp
@@ -5,19 +5,46 @@ MatGGX::MatGGX(const vec3 &Kd, float alpha, float ior) : Kd_(Kd), usetex_Kd_(fal
 vec3 MatGGX::sampleBxdf(const vec3 &wo, const vec3 &normal) const
 {
     // Naive
-    float r2 = rand() * 1.0 / RAND_MAX * 0.99f;
-    float phi = rand() * 1.0 / RAND_MAX * 3.14159 * 2;
-    float r = sqrt(r2);
-    float h = sqrt(1 - r2);
+    // float r2 = rand() * 1.0 / RAND_MAX * 0.99f;
+    // float phi = rand() * 1.0 / RAND_MAX * 3.14159 * 2;
+    // float r = sqrt(r2);
+    // float h = sqrt(1 - r2);
+
+    // vec3 ax0 = abs(normal[0]) < 0.8 ? vec3(1.0f, 0.0f, 0.0f) : vec3(0.0f, 1.0f, 0.0f);
+    // vec3 ax1 = ax0.cross(normal).normalized();
+    // vec3 ax2 = normal.cross(ax1).normalized();
+    // vec3 wi = h * normal + r * cos(phi) * ax1 + r * sin(phi) * ax2;
+    // if (rand() % 2)
+    //     wi = -wi;
+
+    // return wi;
 
     vec3 ax0 = abs(normal[0]) < 0.8 ? vec3(1.0f, 0.0f, 0.0f) : vec3(0.0f, 1.0f, 0.0f);
     vec3 ax1 = ax0.cross(normal).normalized();
     vec3 ax2 = normal.cross(ax1).normalized();
-    vec3 wi = h * normal + r * cos(phi) * ax1 + r * sin(phi) * ax2;
-    if (rand() % 2)
-        wi = -wi;
 
-    return wi;
+    float r1 = rand() * 1.0f / RAND_MAX;
+    float r2 = rand() * 1.0f / RAND_MAX;
+    // float cos_theta = sqrt((1 - r1) / (1 + (alpha_ * alpha_ - 1) * r1));
+    // float theta = acos(cos_theta) * 0.9999;
+    float theta = atan(alpha_ * sqrt(r1 / (1 - r1))) * 0.9999;
+    float phi = 2.0f * 3.14159f * r2;
+    vec3 h = cos(theta) * normal + sin(theta) * cos(phi) * ax1 + sin(theta) * sin(phi) * ax2;
+    vec3 wi;
+
+    if (rand() % 2)
+    {
+        // reflect
+        wi = 2.0f * h.dot(wo) * h - wo;
+    }
+    else
+    {
+        // refract
+        float eta = wi.dot(normal) > 0 ? ior_ : 1.0f / ior_; // eta_o / eta_i
+        float norm_h = -(eta + 1.0f) * wo.dot(h);
+        wi = -eta * wo - h * norm_h;
+    }
+    return wi.normalized();
 }
 
 vec3 MatGGX::F(const vec3 &v, const vec3 &m, const vec3 &Kd) const
@@ -51,7 +78,6 @@ float MatGGX::D(const vec3 &m, const vec3 &n) const
 
 vec3 MatGGX::fr(const vec3 &i, const vec3 &o, const vec3 &n, const vec3 &Kd) const
 {
-    return 0.0f;
     vec3 h = (i + o).normalized();
     vec3 nn = i.dot(n) > 0 ? n : -n;
     vec3 f = F(i, h, Kd);
@@ -66,8 +92,6 @@ vec3 MatGGX::ft(const vec3 &i, const vec3 &o, const vec3 &n, const vec3 &Kd) con
     float eta = i.dot(n) > 0 ? ior_ : 1.0f / ior_; // eta_o / eta_i
     vec3 h = -(i + eta * o).normalized();
     float A = abs(i.dot(h) * o.dot(h) / i.dot(n) / o.dot(n));
-    vec3 nn = i.dot(n) > 0 ? n : -n;
-    vec3 hh = i.dot(h) > 0 ? h : -h;
     vec3 f = F(i, h, Kd);
     vec3 g = G(i, o, h, n);
     vec3 d = D(h, n);
@@ -95,13 +119,22 @@ vec3 MatGGX::bxdf(const vec3 &wo, const vec3 &normal, const vec3 &wi, const vec3
 float MatGGX::pdf(const vec3 &wo, const vec3 &normal, const vec3 &wi) const
 {
     // Naive
-    return 1.0 / 3.14159 * (abs(wi.dot(normal)) + 1e-4f) / 2.0;
-
-    // if (wi.dot(normal) <= 1e-4 || wo.dot(normal) <= 1e-4f)
-    //     return 1e18f;
-    // vec3 wh = (wo + wi).normalized();
-    // float D = pow(alpha_, 2) / 3.14159f / pow(pow(normal.dot(wh), 2) * (pow(alpha_, 2) - 1) + 1, 2);
-    // return D * normal.dot(wh) / 4 + 1e-8f;
+    // return 1.0 / 3.14159 * (abs(wi.dot(normal)) + 1e-4f) / 2.0;
+    if (wi.dot(normal) * wo.dot(normal) > 0)
+    {
+        // reflect
+        vec3 wh = (wo + wi).normalized();
+        float D = pow(alpha_, 2) / 3.14159f / pow(pow(normal.dot(wh), 2) * (pow(alpha_, 2) - 1) + 1, 2);
+        return D * normal.dot(wh) / 4 + 1e-8f * 0.5f;
+    }
+    else
+    {
+        // refract
+        float eta = wo.dot(normal) > 0 ? ior_ : 1.0f / ior_; // eta_o / eta_i
+        vec3 wh = -(wi + eta * wo).normalized();
+        float D = pow(alpha_, 2) / 3.14159f / pow(pow(normal.dot(wh), 2) * (pow(alpha_, 2) - 1) + 1, 2);
+        return D * normal.dot(wh) / 4 + 1e-8f * 0.5f;
+    }
 }
 
 bool MatGGX::isEmission() const
