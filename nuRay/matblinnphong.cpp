0d834ba38b34ea921c72b2b161cd9f515364760b
mollnn <1084918073@qq.com>
2022-03-01 18:40:17 +0800

    fix: blinn phong

--- nuRay/matblinnphong.cpp
+++ nuRay/matblinnphong.cpp
@@ -4,7 +4,7 @@ MatBlinnPhong::MatBlinnPhong(const vec3 &Kd, const vec3 &Ks, float Ns) : Kd_(Kd)
 
 vec3 MatBlinnPhong::sampleBxdf(const vec3 &wo, const vec3 &normal) const
 {
-    float lambda = 0.3f;
+    float lambda = 0.5f;
     float r0 = rand() * 1.0f / RAND_MAX;
 
     vec3 ax0 = abs(normal[0]) < 0.8 ? vec3(1.0f, 0.0f, 0.0f) : vec3(0.0f, 1.0f, 0.0f);
@@ -24,11 +24,12 @@ vec3 MatBlinnPhong::sampleBxdf(const vec3 &wo, const vec3 &normal) const
     }
     else
     {
-        float r1 = rand() * 1.0f / RAND_MAX;
-        float r2 = rand() * 1.0f / RAND_MAX;
-        float theta = acos(pow(r1, 1.0f / (Ns_ + 2.0f)));
-        float phi = 2.0f * 3.14159f * r2;
-        vec3 h = cos(theta) * normal + sin(theta) * cos(phi) * ax1 + sin(theta) * sin(phi) * ax2;
+        double r1 = rand() * 1.0f / RAND_MAX;
+        double r2 = rand() * 1.0f / RAND_MAX;
+        double cos_theta = pow(r1, 1.0f / (Ns_ + 2.0f));
+        double sin_theta = sqrt(1 - cos_theta);
+        double phi = 2.0f * 3.14159f * r2;
+        vec3 h = cos_theta * normal + sin_theta * cos(phi) * ax1 + sin_theta * sin(phi) * ax2;
         vec3 wi = 2.0f * h.dot(wo) * h - wo;
         return wi;
     }
@@ -36,26 +37,24 @@ vec3 MatBlinnPhong::sampleBxdf(const vec3 &wo, const vec3 &normal) const
 
 vec3 MatBlinnPhong::bxdf(const vec3 &wo, const vec3 &normal, const vec3 &wi, const vec3 &uv) const
 {
+    if (wi.dot(normal) <= 1e-4 || wo.dot(normal) <= 1e-4f)
+        return 0.0f;
     vec3 Rd = usetex_Kd_ ? map_Kd_.pixelUV(uv[0], uv[1]) : Kd_;
     vec3 Rs = usetex_Ks_ ? map_Ks_.pixelUV(uv[0], uv[1]) : Ks_;
     vec3 diffuse = Rd / 3.14159;
     vec3 h = (wo + wi).normalized();
-    vec3 specular = (2.0f + Ns_) / (4.0f * 3.14159f * (2.0f - pow(2.0f, -0.5f * Ns_))) * Rs * pow(normal.dot(h), Ns_);
+    vec3 specular = (Ns_ + 2) / 3.14159 / 8 * Rs * pow(normal.dot(h), Ns_);
     return diffuse + specular;
 }
 
 float MatBlinnPhong::pdf(const vec3 &wo, const vec3 &normal, const vec3 &wi) const
 {
-    // Todo: merge
     if (wi.dot(normal) <= 1e-4 || wo.dot(normal) <= 1e-4f)
         return 1e18f;
-    float lambda = 0.3f;
+    float lambda = 0.5f;
     float pdf_diffuse = 1.0 / 3.14159 * (wi.dot(normal) + 1e-6f);
     vec3 h = (wo + wi).normalized();
-    float cos_h = normal.dot(h);
-    float sin_h = sqrt(1.0f - cos_h * cos_h);
-    float pdf_specular_h = (Ns_ + 2.0f) * pow(cos_h, Ns_ + 1) * sin_h;
-    float pdf_specular = pdf_specular_h / 4.0f / wo.dot(h) / 2.0f / 3.14159f / (h.cross(normal).norm() + 1e-6f);
+    float pdf_specular = (Ns_ + 2) / 3.14159 / 8 * pow(normal.dot(h), Ns_ + 1);
     return lambda * pdf_diffuse + (1.0f - lambda) * pdf_specular;
 }
 
